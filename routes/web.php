1265b490b1fe5a730653033f5d298210d4b7d25b
oneautumn999 <ypmilala20@gmail.com>
2023-03-28 03:33:13 +0700

    feature: Login and Register

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+//phpcs:disable
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -14,18 +14,29 @@ use Inertia\Inertia;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::redirect('/', '/prototype/login');
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
+Route::get(
+    '/dashboard',
+    function () {
+        return Inertia::render('Dashboard');
+    }
+)->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::prefix('prototype')->name('prototype.')->group(
+    function () { 
+        Route::get(
+            '/login', function () {
+                return Inertia::render('Prototype/Login');
+            }
+        )->name('login');
+        Route::get(
+            '/register', function () {
+                return Inertia::render('Prototype/Register');
+            }
+        )->name('register');
+    }
+);
 
-require __DIR__.'/auth.php';
+
+require __DIR__ . '/auth.php';
